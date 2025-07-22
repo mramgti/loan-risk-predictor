cc122029ce2f6a72ea5d953e2a2b7d12ab790fda
Maycon Ramos <maycon_marchitto@hotmail.com>
2025-07-22 15:18:30 -0300

    Versão inicial do projeto com Prefect e Streamlit

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -230,6 +230,8 @@ from time import sleep
 import pandas as pd
 import numpy as np
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flows.loan_flow import loan_flow
 
 import streamlit as st
